72bcd8b04b280f868b8afcf9c0ab03f683d3f3c5
Иван Кульков <96732372+DisurIE@users.noreply.github.com>
2022-07-03 18:33:46 +0400

    form update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+"use strict"
     const burger = document.querySelector('.menu-icon');
     const burgerClose = document.querySelector('.menu-close');
     burger.addEventListener('mouseover', () => {
@@ -69,3 +69,101 @@
         $.fn.pagepiling.moveTo('4');
     });
     
+
+        document.addEventListener('DOMContentLoaded', function (){
+            const form = document.getElementById('form');
+            form.addEventListener('submit', formSend);
+
+            async function formSend(e){
+                e.preventDefault();
+
+                let error = formValidate(form);
+
+                let formData = new FormData(form);
+                    formData.append('image', formImage.files[0]);
+
+                if(error === 0){
+                    form.classList.add('_sending');
+                    let response = await fetch('sendmail.php', {
+                        method: 'POST',
+                        body: formData
+                    });
+                    if(response.ok){
+                        let result = await response.json();
+                        alert(result.message);
+                        formPreview.innerHTML = '';
+                        form.reset();
+                        form.classList.remove('_sending');
+                    }
+                    else{
+                        form.classList.remove('_sending');
+                        alert('ошибка');
+                    }
+                }
+                else{
+                    alert('Заполните обязательные поля');
+                }
+
+            }
+
+            function formValidate(){
+                let error = 0;
+                let formReq = document.querySelectorAll('._req');
+
+
+                for(let index = 0; index < formReq.length; index++){
+                    const input = formReq[index];
+                    formRemoveError(input);
+
+                    if(input.classList.contains('contacts__mail')){
+                        if(emailTest(input)){
+                            formAddError(input);
+                            error++;
+                        }
+                        else if(input.getAttribute("type") === "checkbox" && input.checked === false){
+                            formAddError(input);
+                            error++;
+                        }
+                        else{
+                            if(input.value === ''){
+                                formAddError(input);
+                                error++;
+                            }
+                        }
+                    }
+
+                }
+                return error;
+            }
+
+            function formAddError(input){
+                input.classList.add('_error');
+            }
+            function formRemoveError(input){
+                input.classList.remove('_error');
+            }
+            function emailTest(input){
+                return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+            }
+
+            const formImage = document.getElementById('fileImage');
+
+            formImage.addEventListener('change', () => {
+                uploadFile(formImage.files[0]);
+            });
+
+            function uploadFile(file){
+                if(!['image/jpeg', 'image/png', 'image/fig', 'image/gif', 'image/psd', 'image/webp'].includes(file.type)){
+                    alert('Разрешены только изображения');
+                    formImage.value = '';
+                    return;
+                }
+                if(file.size > 20 * 1024 * 1024){
+                    alert('Файл должен быть не больше 20мб');
+                    return;
+                }
+
+                var reader = new FileReader();
+                reader.readAsDataURL(file);
+            }
+        });
---
+++ sendmail.php
@@ -0,0 +1,56 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->setLanguage('ru', 'PHPMailer-6.6.6/language/');
+    $mail->isHTML(true);
+
+    $mail->setFrom('disurfl@bk.ru', 'Пользователь фрик');
+
+    $mail->addAddress('kulkov_2003@bk.ru');
+
+    $mail->Subject = 'Привет это я твой клиент';
+
+    $body = '<h1> Это твой новый проект </h1>';
+
+    if(trim(!empty($_POST['name']))){
+        $body.='<p>ИМЯ:'.$_POST['name'].'</p>';
+    }
+    if(trim(!empty($_POST['company']))){
+        $body.='<p>Компания:'.$_POST['company'].'</p>';
+    }
+    if(trim(!empty($_POST['mail']))){
+        $body.='<p>MAIL:'.$_POST['mail'].'</p>';
+    }
+    if(trim(!empty($_POST['description']))){
+        $body.='<p>Описание:'.$_POST['description'].'</p>';
+    }
+    if(!empty($_FILES['image']['tmp_name'])){
+        $filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
+
+        if(copy($_FILES['image']['tmp_name'], $filePath)){
+            $fileAttach = $filePath;
+            $body.='<p>Макет в приложении</p>';
+            $mail->addAttachment($fileAttach);
+        }
+    }
+
+    $mail->Body = $body;
+
+    if (!$mail->send()){
+        $message = 'ошибка';
+    }
+    else{
+        $message = 'Данные отправлены';
+    }
+
+    $response = ['message' => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
+?>
